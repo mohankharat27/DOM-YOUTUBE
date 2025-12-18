a69ed4da628a8f70a2294c4bce32530c4ba8cf0b
mohankharat27 <kharatmohan2244@gmail.com>
2025-12-18 19:53:25 +0530

    event capturing

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let ul = document.querySelector("ul")
+let a = document.querySelector("#a");
+let b = document.querySelector("#b");
+let c = document.querySelector("#c");
+let btn = document.querySelector("button");
 
-ul.addEventListener("click",function(dets){
-    dets.target.style.textDecoration =  "line-through";
-})
+a.addEventListener("click",()=>{
+    console.log("a clicked")
+},true);
+
+b.addEventListener("click",()=>{
+    console.log("b clicked")
+});
+
+c.addEventListener("click",()=>{
+    console.log("c clicked")
+},true);
+
+btn.addEventListener("click",()=>{
+    console.log("button clicked")
+});
